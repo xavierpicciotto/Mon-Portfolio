f0fb436e9b3eff73ca22af1f1d85ce153688fc5d
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-01-27 18:22:26 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background-image: -webkit-linear-gradient(0deg,#766dff 0%,#88f3ff 100%);
   }
-}
+
+  #app {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background-color: rgba(12, 2, 3, 0.068);
+    color: rgb(255, 255, 255); 
+  }
 </style>
