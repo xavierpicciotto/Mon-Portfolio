ea50083b5aa6d2bf7163b7fd06ca79ee13a304d0
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-01-30 00:26:29 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="home">
     <header>
-      <div class="container">
-        <img alt="Une photo de xavier picciotto" src="../assets/Me.jpg">
-        <div class="name">
-          <h1 class="police_lobster">Xavier Picciotto</h1>
-          <p>Développeur Web</p>
-        </div>
-        <div class="link-container">
-          <a href="https://github.com/xavierpicciotto?tab=repositories" target="blank">
-            <i class="fab fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/xavier-picciotto-developpeur-web/" target="blank">
-            <i class="fab fa-linkedin"></i>
-          </a>
-        </div>
+      <img alt="Une photo de xavier picciotto" src="../assets/Me.jpg">
+      <div class="name">
+        <h1 class="police_lobster">Xavier Picciotto</h1>
+        <p>Développeur Web</p>
+      </div>
+      <div class="link-container">
+        <a href="https://github.com/xavierpicciotto?tab=repositories" target="blank">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/xavier-picciotto-developpeur-web/" target="blank">
+          <i class="fab fa-linkedin"></i>
+        </a>
       </div>
     </header>
     <div class="animed-point">
@@ -41,7 +39,8 @@
           <p class="police_prompt">Je suis récemment diplômé chez OpenClassrooms d'un titre RNCP (Bac +2) de développeur
             Web Junior. Cette
             formation en autonomie m’a permis d'acquérir des connaissances dans le développement web notamment sur les
-            langages <strong>HTML, CSS, Javascript, MySQL, référencement</strong> et le framework <strong>Vue</strong>. je suis aussi
+            langages <strong>HTML, CSS, Javascript, MySQL, référencement</strong> et le framework <strong>Vue</strong>.
+            je suis aussi
             ouvert à l'apprentissage de nouveaux
             langages et technologies.</p>
           <p class="police_prompt">Je suis capable de créer et améliorer des sites internet, d'utiliser des API.
@@ -56,18 +55,46 @@
           <h3 class="police_lobster">Mon CV</h3>
           <div class="img-box">
             <img src="../assets/CV.jpg" alt="Mon CV de développeur web format jpg">
-            <a href="../assets/Xavier-Picciotto_CV.pdf" download class="police_prompt">Télécharger <i class="fas fa-download"></i></a>
+            <a href="../assets/Xavier-Picciotto_CV.pdf" download class="police_prompt">Télécharger <i
+                class="fas fa-download"></i></a>
           </div>
         </div>
-        <div class="contact police_lobster">Contactez moi</div>
+        <div class="contact ">
+          <h3 class="police_lobster">Contactez moi</h3>
+          <form class="form_contact police_prompt" id="form_contact">
+            <label for="firstName">Prénom* :</label>
+            <input v-model="contact.firstName" type="text" name="firstName" placeholder="Prénom (requis)" id="firstName"
+              pattern="[a-zA-z/ /-]+" required>
+            <label for="lastName">Nom :</label>
+            <input v-model="contact.lastName" type="text" name="lastName" placeholder="Nom (facultatif)" id="lastName"
+              pattern="[a-zA-z/ /-]+">
+            <label for="entreprise">Entreprise :</label>
+            <input v-model="contact.entreprise" type="text" name="entreprise" id="entreprise"
+              placeholder="Entreprise (facultatif)" pattern="[a-zA-z/ /-]+">
+            <label for="email">E-mail* :</label>
+            <input v-model="contact.email" type="email" name="email" placeholder="exemple@domaine.com (requis)"
+              id="email" required>
+            <label for="message">Message* :</label>
+            <textarea v-model="contact.message" id="address" name="message" rows="4"
+              placeholder="Vous pouvez m'écrire ici (requis)." required></textarea>
+            <button type="submit" class="form_button" id="submit">Envoyer</button>
+          </form>
+        </div>
       </section>
     </main>
   </div>
 </template>
 
 <script>
+  import Contact from '../models/contact';
   export default {
     name: 'Home',
+    data() {
+      return {
+        contact: new Contact('', '', '', '', ''),
+        successfull: false
+      }
+    }
   }
 </script>
 
@@ -81,47 +108,46 @@
   }
 
   header {
-    text-align: center;
     background-image: url('../assets/cloud.jpg');
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
     border-radius: 0 0 20% 20%;
-
-
-    .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
     .name {
       display: flex;
       flex-direction: column;
+      justify-content: center;
+      align-items: center;
       font-size: 2vw;
       margin: auto;
       text-shadow: 2px 5px 5px black;
-      animation: pirouette 10s infinite alternate ease-in-out;
+      animation: pendulum 2s infinite alternate ease-in-out;
     }
 
     .link-container {
-      margin: 10px;
+      width: 14%;
+      display: flex;
+      justify-content: space-around;
 
       i {
-        color: white;
-        margin: 40px;
         font-size: 3vw;
         text-shadow: 3px 3px rgba(0, 0, 0, 0.534);
         transition: 0.3s ease-out;
-        &:hover{
-         transform: translate(0px, -7px);
-         text-shadow: 3px 10px rgba(0, 0, 0, 0.534);
+
+        &:hover {
+          transform: translate(0px, -7px);
+          text-shadow: 3px 10px rgba(0, 0, 0, 0.534);
         }
       }
     }
 
     img {
-      margin: 10px 50px;
+      margin: 0;
+      min-width: 100px;
       width: 15%;
       height: auto;
       border-radius: 50%;
@@ -186,6 +212,10 @@
         height: 100px;
         border-radius: 15%;
         box-shadow: 4px 6px 3px rgba(0, 0, 0, 0.37);
+
+        &:hover {
+          animation: newFocus 0.6s forwards ease-in-out;
+        }
       }
     }
   }
@@ -195,7 +225,7 @@
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.623);
 
     .presentation {
-      margin: 2% 10%;
+      margin: 5% 10%;
     }
 
     .cv {
@@ -204,7 +234,8 @@
       .img-box {
         width: 200px;
         height: auto;
-        a{
+
+        a {
           text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.493);
         }
 
@@ -229,6 +260,139 @@
     }
   }
 
+  .contact {
+    margin: 5% 10%;
+
+    form {
+      margin: auto 5%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      label {
+        margin: auto 10%;
+      }
+
+      input {
+        margin: 2% 20%;
+        font-size: 1em;
+        border-radius: 5px;
+        border-style: none;
+        outline: none;
+        box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.424);
+
+        &:focus {
+          animation: newFocus 0.6s forwards ease-in-out;
+        }
+      }
+
+      textarea {
+        outline: none;
+        margin: 2% 10%;
+        border-radius: 5px;
+        font-size: 1em;
+
+        &:focus {
+          animation: newFocus 0.6s forwards ease-in-out;
+        }
+      }
+
+      button {
+        margin: 4% auto;
+        box-shadow: inset 0px 1px 0px 0px #a4e271;
+        background: linear-gradient(to bottom, #89c403 5%, #77a809 100%);
+        background-color: #89c403;
+        border-radius: 6px;
+        border: 1px solid #74b807;
+        display: inline-block;
+        cursor: pointer;
+        color: #ffffff;
+        font-family: Arial;
+        font-size: 20px;
+        font-weight: bold;
+        padding: 6px 24px;
+        text-decoration: none;
+        text-shadow: 0px 1px 0px #528009;
+        animation: jumps 1s infinite alternate-reverse ease-in-out;
+
+        &:hover {
+          background: linear-gradient(to bottom, #77a809 5%, #89c403 100%);
+          background-color: #77a809;
+        }
+
+        &:active {
+          position: relative;
+          top: 1px;
+        }
+      }
+    }
+  }
+
+  //--Responsive--
+  @media only screen and (max-width: 900px) {
+    header {
+      flex-direction: column;
+      border-radius: 10px;
+
+      .name {
+        font-size: 3.5vh;
+
+        h1 {
+          margin: 0;
+        }
+      }
+
+      img {
+        margin-top: 5%;
+        width: 30%;
+      }
+
+      .link-container {
+        width: 100%;
+        margin-bottom: 1%;
+        i {
+          font-size: 2.5em;
+        }
+      }
+    }
+
+    .animed-point {
+      .point {
+        margin: 5px auto;
+      }
+    }
+
+    .skills {
+      h2 {
+        font-size: 4.5vh;
+      }
+
+      .skills_box {
+        img {
+          width: 65px;
+          height: 65px;
+        }
+      }
+
+    }
+    .personal{
+      .contact{
+        form{
+         margin: auto; 
+        }
+        label{
+          margin: auto 0;
+        }
+        input{
+          margin: 2% 0;
+        }
+        textarea{
+          margin: 2% 0;
+        }
+      }
+    }
+  }
+
   //---Animations---
   @keyframes blinks {
     from {
@@ -239,22 +403,48 @@
       box-shadow: #FFF 0px 0px 5px, #1c24d7 0px 0px 10px, #FFF 0px 0px 15px, #73d8f6 0px 0px 20px, #3e9bf1 0px 0px 30px, #36bee7 0px 0px 40px, #4f68f6 0px 0px 50px, #fafafa 0px 0px 75px;
     }
   }
+
   @keyframes jumps {
     from {
-      transform: translate(0px , 0px);
+      transform: translate(0px, 0px);
     }
 
     to {
-      transform: translate(0px , 7px);
+      transform: translate(0px, -7px);
     }
   }
-    @keyframes pirouette {
+
+  @keyframes pendulum {
     from {
-      transform: rotateX(0deg);
+      transform: translate(0px, 0px) scale(1);
     }
 
     to {
-      transform: rotateX(360deg);
+      transform: translate(0px, 5px) scale(1.005);
     }
   }
+
+  @keyframes newFocus {
+    from {
+      transform: scale(1);
+    }
+
+    to {
+      transform: scale(1.10);
+      box-shadow: 2px 10px 3px rgba(0, 0, 0, 0.424);
+    }
+  }
+  @-webkit-keyframes tracking-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
+  40% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
 </style>
