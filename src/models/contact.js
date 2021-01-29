bd26a75aa0b8df77ff86130cb56cdc7f58dd9380
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-01-30 00:26:26 +0100

    Create contact.js

---
+++ src/models/contact.js
@@ -0,0 +1,9 @@
+export default class Contact {
+    constructor(firstName, lastName, email,entreprise , message ) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.entreprise = entreprise;
+      this.email = email;
+      this.message = message;
+    }
+  }
